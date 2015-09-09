34eb0bade117de99f00ba498833ee6813bd929f7
Jennifer Hung <jennifersukay505@gmail.com>
2015-09-09 16:24:03 -0700

    index and gitignore

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index Page for SQL and PHP Practice Files</title>
+    </head>
+    <body>
+        <p> SQL and PHP Practice</p>
+    </body>
+</html>
